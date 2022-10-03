86fab7353d993eeaace36761175f7e9560180ef4
mohamedtaher <mt@prollygeek.com>
2022-10-03 16:32:43 +0200

    remove artifacts api

--- src/artifacts/artifacts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ArtifactsService } from './artifacts.service';
-import { ArtifactsController } from './artifacts.controller';
-
-@Module({
-  controllers: [ArtifactsController],
-  providers: [ArtifactsService]
-})
-export class ArtifactsModule {}
--- src/artifacts/dto/update-artifact.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateArtifactDto } from './create-artifact.dto';
-
-export class UpdateArtifactDto extends PartialType(CreateArtifactDto) {}
